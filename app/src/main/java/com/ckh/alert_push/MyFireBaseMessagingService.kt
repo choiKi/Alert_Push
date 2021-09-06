ea38a43481903e9c2b8ae404575db6c521b766b0
ChoiKi <rlgns923@naver.com>
2021-09-06 15:08:55 +0900

    d

--- app/src/main/java/com/ckh/alert_push/MyFireBaseMessagingService.kt
+++ app/src/main/java/com/ckh/alert_push/MyFireBaseMessagingService.kt
@@ -1,6 +1,7 @@
 package com.ckh.alert_push
 
 
+import android.os.Build
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -12,5 +13,10 @@ class MyFireBaseMessagingService:FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+    }
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.0){
+
+        }
     }
 }
